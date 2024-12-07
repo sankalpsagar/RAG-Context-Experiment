93ca97d534048e21499de62bc4d9e0b6028d7476
Samuel Karskens <s.m.f.karskens@student.tudelft.nl>
2024-12-07 22:40:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+download dataset from:
+https://drive.google.com/drive/folders/1qIZcNcU2wtiJNr3BUyX2GIUtnHEfbQDi?usp=sharing
+
+for retrieving contexts:
+https://drive.google.com/drive/folders/1aQAfNLq6HB0w4_fVnKMBvKA6cXJGRTpH?usp=sharing
+
+some links:
+https://pypi.org/project/dexter-cqa/
+https://anonymous.4open.science/r/BCQA-05F9/README.md
+
+
+# What I had to do to get it working on Ubuntu 24.04.1
+use anaconda env with python 3.11
---
+++ example_from_dexter.py
@@ -0,0 +1,35 @@
+from dexter.config.constants import Split
+from dexter.data.datastructures.hyperparameters.dpr import DenseHyperParams
+from dexter.data.loaders.RetrieverDataset import RetrieverDataset
+from dexter.retriever.dense.ANCE import ANCE
+from dexter.retriever.dense.Contriever import Contriever
+from dexter.utils.metrics.SimilarityMatch import CosineSimilarity
+from dexter.utils.metrics.retrieval.RetrievalMetrics import RetrievalMetrics
+
+if __name__ == "__main__":
+    # Ensure in config.ini the path to the raw data files are linked under [Data-Path]
+    # ambignq = '<path to the data file>
+    # ambignq-corpus = '<path to the corpus file>'
+
+    # You can set the split to one of Split.DEV, Split.TEST or Split.TRAIN
+    # Setting tokenizer=None only loads only the raw data processed into our standard data classes, if tokenizer is set, the data is also tokenized and stored in the loader.
+    loader = RetrieverDataset("ambignq","ambignq-corpus",
+                              "config.ini", Split.DEV,tokenizer=None)
+
+    # Initialize your retriever configuration
+    config_instance = DenseHyperParams(query_encoder_path="facebook/contriever",
+                                       document_encoder_path="facebook/contriever"
+                                       ,batch_size=32,show_progress_bar=True)
+
+    # From data loader loads list of queries, corpus and relevance labels.
+    queries, qrels, corpus = loader.qrels()
+
+    #Perform Retrieval
+    contrvr_search = Contriever(config_instance)
+    similarity_measure = CosineSimilarity()
+    response = contrvr_search.retrieve(corpus,queries,100,similarity_measure,chunk=True,chunksize=400000)
+
+
+    #Evaluate retrieval metrics
+    metrics = RetrievalMetrics(k_values=[1,10,100])
+    print(metrics.evaluate_retrieval(qrels=qrels,results=response))
