3d34d2693fc7db9b1e4b114492f485958fc137a3
samuel <s.m.f.karskens@student.tudelft.nl>
2025-01-12 18:40:14 +0100

    add some tsv and json files

--- ADORE/preprocess_dev_qrels2.py
+++ ADORE/preprocess_dev_qrels2.py
@@ -3,10 +3,10 @@ import json
 import csv
 
 data = None
-with open('../output_json_qrels_train2.json', 'r') as file:
+with open('../output_json_qrels_dev.json', 'r') as file:
     data = json.load(file)
 
-with open('../output_query_id_to_new_id_train2.json', 'r') as file:
+with open('./output_query_id_to_new_id_dev.json', 'r') as file:
     query_id_to_new_id = json.load(file)
 
 
@@ -23,7 +23,7 @@ for index, item in enumerate(data):
             'relevance': value,
         })
 # <query_id> 0 <doc_id> <relevance>
-with open('output_qrels_train2.tsv', 'w') as output_file:
+with open('output_qrels_dev.tsv', 'w') as output_file:
     dict_write = csv.DictWriter(output_file, data_changed[0].keys(), delimiter='\t')
     # dict_write.writeheader()
     dict_write.writerows(data_changed)
--- dev_qrels_to_json.py
+++ dev_qrels_to_json.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     # You can set the split to one of Split.DEV, Split.TEST or Split.TRAIN
     # Setting tokenizer=None only loads only the raw data processed into our standard data classes, if tokenizer is set, the data is also tokenized and stored in the loader.
     loader = RetrieverDataset("wikimultihopqa", "wikimultihopqa-corpus",
-                              "config.ini", Split.TRAIN, tokenizer=None)
+                              "config.ini", Split.DEV, tokenizer=None)
 
     # Initialize your retriever configuration
     # config_instance = DenseHyperParams(query_encoder_path="facebook/contriever",
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     for q in queries:
         array_queries.append(q.id())
 
-    with open('output_json_qrels_train2.json', 'w') as output_file:
+    with open('output_json_qrels_dev.json', 'w') as output_file:
         json.dump(qrels, output_file)
-    with open('output_json_queries_train2.json', 'w') as output_file:
+    with open('output_json_queries_dev.json', 'w') as output_file:
         json.dump(array_queries, output_file)
         # dict_write = csv.DictWriter(output_file, data_changed[0].keys(), delimiter='\t')
     # dict_write.writeheader()
